50a1085250b92bb018ee85c50383ff841e6ccf44
JoseMaria16 <josejaureguirivas@gmail.com>
2023-09-27 23:29:15 -0500

    ssds

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -12,12 +12,12 @@ function Header() {
             <div className='bg-black w-screen -ml-16 lg:-ml-32 h-4'></div>
             <header className='flex flex-col   pt-6 pb-0'>
                 <div className='flex  justify-start gap-10 items-center lg:justify-center '>
-                    <button className=' flex lg:hidden' onClick={() => setOpen(true)}>X</button>
+                    <button className=' flex lg:hidden' onClick={() => {}}>X</button>
                     <h2 className={` text-3xl lg:text-5xl text-center font-semibold`}>Made In Camaná</h2>
                     
                 </div>
                 <div className='flex lg:hidden'>
-                <NavBar open={open} setOpen={setOpen}  />
+                {/* <NavBar open={open}  /> */}
                 </div>
             </header >
         </div>
--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -5,9 +5,9 @@ import React, {useState} from 'react'
 function NavBar({open, setOpen}) {
     
     return (
-        <nav className={`fixed ${open ? 'left-0' : '-left-[1000px]'} left-0 h-screen lg:h-max w-1/2 lg:sticky top-0 z-10 lg:w-full border-b-2 bg-white border-gray-300 pt-5 mb-5 pb-4`}>
+        <nav className={`fixed  -left-[1000px] h-screen lg:h-max w-1/2 lg:sticky top-0 z-10 lg:w-full border-b-2 bg-white border-gray-300 pt-5 mb-5 pb-4`}>
             <ul className='flex flex-col lg:flex-row lg:justify-between px-10 gap-5 lg:items-center'>
-                <div className='bg-black w-5 h-5 rounded-full' onClick={() => setOpen(false)}></div>
+                <div className='bg-black w-5 h-5 rounded-full'></div>
 
                 <li>
                     <Link href={'/'}>INICIO</Link>
